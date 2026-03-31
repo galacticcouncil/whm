eb58d43131f3a521e0ed66dd5618b9e34e0c6da2
mrq <mrq@mrq.cz>
2026-03-31 02:18:41 +0200

    migrate to fork

--- platforms/evm/sh/migrate-basejump.sh
+++ platforms/evm/sh/migrate-basejump.sh
@@ -26,6 +26,9 @@ ENV=${1:?Usage: migrate-basejump.sh <env>}
 
 if [ "$ENV" = "fork" ]; then
   PROXY_ENV="fork"
+  PK=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+  PK_PROXY=$PK
+  PK_LANDING=$PK
 else
   PROXY_ENV="moon"
 fi
